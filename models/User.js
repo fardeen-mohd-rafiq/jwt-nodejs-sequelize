04e0309c558285aee0338f18acdf30dae4440b66
fardeen <s.fardeen@ilmux.com>
2024-05-29 19:19:41 +0530

    feat: jwt implementation

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+module.exports = function(sequelize) {
+    const User = sequelize.define('user', {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+
+    return User
+}
